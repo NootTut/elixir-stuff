90e22ff51f469d0dfd43655f7f544343745adccb
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-28 17:32:32 +0000

    echo command

--- lib/elixir_stuff.ex
+++ lib/elixir_stuff.ex
@@ -7,9 +7,14 @@ defmodule ElixirStuff do
   end
 
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
-    case msg.content do
-      "ping!" -> Api.create_message(msg.channel_id, "running on elixir and nostrum <3")
-      _ -> :ignore
+    if msg.author().bot() do
+      :ignored
+    else
+      [head | tail] = msg.content() |> String.split()
+      case head |> String.downcase() do
+       "echo" -> msg.channel_id() |> Api.create_message(tail |> Enum.join(" "))
+        _ -> :ignored
+      end
     end
   end
 
