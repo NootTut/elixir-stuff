62aa08de31c452aa23b9630a28218c6192a81909
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-29 18:48:02 +0000

    cleaner syntax

--- lib/commands/command_manager.ex
+++ lib/commands/command_manager.ex
@@ -9,21 +9,21 @@ defmodule ElixirStuff.Commands do
   end
 
   def handle_event({:READY, {_map}, _ws_state}) do
-    Enum.each(__MODULE__.__info__(:functions), fn {val, _arity} ->
+    for {val, _arity} <- __MODULE__.__info__(:functions) do
       str_name = Atom.to_string(val)
       unless !String.starts_with?(str_name, "on_") do
         name = String.slice(str_name, 3..String.length(str_name))
         Logger.info "Adding #{name} command!"
-        Agent.update(__MODULE__, fn state -> Map.put(state, "!#{name}", fn msg -> spawn fn -> Logger.info("Executing #{name}")apply(__MODULE__, val, [msg]) end end) end)
+        Agent.update(__MODULE__, fn state -> Map.put(state, "!#{name}", fn msg -> spawn fn -> apply(__MODULE__, val, [msg]) end end) end)
       end
-    end)
+    end
   end
 
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
     unless !String.starts_with?(msg.content(), "!") do
       [head | _tail] = String.split(msg.content(), " ")
       got = Agent.get(__MODULE__, fn state -> Map.fetch(state, head) end)
-      if got != :error, do: Kernel.elem(got, 1).(msg)
+      if got != :error, do: elem(got, 1).(msg)
     end
   end
 
