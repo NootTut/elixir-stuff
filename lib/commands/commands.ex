7369a26acd38c540ed99d703f17c477b00e4878b
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-29 21:12:45 +0000

    embeds woooo

--- lib/commands/commands.ex
+++ lib/commands/commands.ex
@@ -1,22 +1,47 @@
 defmodule ElixirStuff.Commands.List do
   alias Nostrum.Api
-  def on_ping(msg), do: Api.create_message(msg.channel_id(), "Pong!")
+
+  @owner_id 199_217_346_911_404_032
+  @bot_color 4_224_767
+  @error_color 16_725_555
+  @warning_color 16_736_000
+
+  def on_ping(msg), do: Api.create_message(msg.channel_id(), embed: embed(msg, description: "Pong!"))
 
   def on_eval(msg) do
-    if msg.author().id() != 199217346911404032 do
-      Api.create_message(msg.channel_id(), "You cannot evaluate code!")
+    if msg.author().id() != @owner_id do
+      Api.create_message(msg.channel_id(), embed: embed(msg, description: "You cannot evaluate code!", color: @warning_color))
     else
       [head | _tail] = String.split(msg.content(), " ")
       try do
         {result, _bindings} = msg.content()
                               |> String.slice(String.length(head)..String.length(msg.content()))
                               |> Code.eval_string([msg: msg], __ENV__)
-        Api.create_message(msg.channel_id(), "```#{inspect result}```")
+        embed = embed(msg, description: "Execution succeeded! Here's the result:\n```#{inspect result}```",
+            footer: "Elixir Code evaluated by #{msg.author().username()}\##{msg.author().discriminator()}")
+        Api.create_message!(msg.channel_id(), embed: embed)
       rescue
-        e -> Api.create_message(msg.channel_id(), "An error occurred: #{inspect e}")
+        e ->
+          embed = embed(msg, description: "**An error occurred:** #{e.message}", footer: "Please report this bug to the developers.",
+            color: @error_color)
+          Api.create_message(msg.channel_id(), embed: embed)
       catch
-        e -> Api.create_message(msg.channel_id(), "An error occurred: #{inspect e}")
+        e ->
+          embed = embed(msg, description: "**An error occurred:** #{e.message}", footer: "Please report this bug to the developers.",
+            color: @error_color)
+          Api.create_message(msg.channel_id(), embed: embed)
       end
     end
+  end
+
+  defp embed(msg, opts) do
+    alias Nostrum.Struct.User
+    import Nostrum.Struct.Embed
+
+    %Nostrum.Struct.Embed {}
+    |> put_description(opts[:description])
+    |> put_footer(opts[:footer] || "Requested by #{msg.author().username()}\##{msg.author().discriminator()}" , User.avatar_url(msg.author()))
+    |> put_color(opts[:color] || @bot_color)
+    |> put_timestamp(DateTime.utc_now() |> DateTime.to_iso8601())
   end
 end
